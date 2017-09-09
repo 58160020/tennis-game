a908776eb58c17bf3d0a4476e4e9415376e2c925
Nattanich Toopsamoot <nattanich27@gmail.com>
2017-09-09 12:17:46 +0700

    Test tennis 4 passssss

--- tennis.spec.js
+++ tennis.spec.js
@@ -12,66 +12,129 @@ function TennisGame() {
        return `${scoreString[this.playerAScore]} - ${scoreString[this.playerBScore]}`
     }
 
-    this.playerAGteScore = () => {
+    this.playerAGetScore = () => {
         this.playerAScore++
 
     }
-    this.playerBGteScore = () => {
+    this.playerBGetScore = () => {
         this.playerBScore++
 
     }
 
 }
 
-test('Echo "Love - Love" when game start', () => {
-    // Arrange
-    let app = new TennisGame()
+describe('echo()', () =>{
+ let app
 
-    // Act
+  beforeEach(() => {
+    app = new TennisGame()
     app.reset()
-    let result = app.echo()
+  })
 
-    // Assert
-    expect(result).toBe('Love - Love')
+  test('Echo "Love - Love" when game start', () => {
+      // Arrange
+      // let app = new TennisGame()
+      //
+      // // Act
+      // app.reset()
+      let result = app.echo()
+
+      // Assert
+      expect(result).toBe('Love - Love')
+  })
+
+  test('Echo "Fifteen - Love" when playerA get first score', () => {
+      // Arrange
+      // let app = new TennisGame()
+      // app.reset()
+      app.playerAGetScore()
+
+      // Act
+      let result = app.echo()
+
+      // Assert
+      expect(result).toBe('Fifteen - Love')
+  })
+
+  test('Echo "Love - Fifteen" when playerB get first score', () => {
+      // Arrange
+      // let app = new TennisGame()
+      // app.reset()
+      app.playerBGetScore()
+
+      // Act
+      let result = app.echo()
+
+      // Assert
+      expect(result).toBe('Love - Fifteen')
+  })
+
+  test('Echo "Thirty - Fifteen" when score is 30 - 15 ', () => {
+      // Arrange
+      // let app = new TennisGame()
+      // app.reset()
+      app.playerBGetScore()
+      app.playerAGetScore()
+      app.playerAGetScore()
+
+      // Act
+      let result = app.echo()
+
+      // Assert
+      expect(result).toBe('Thirty - Fifteen')
+  })
+
 })
 
-test('Echo "Fifteen - Love" when playerA get first score', () => {
-    // Arrange
-    let app = new TennisGame()
-    app.reset()
-    app.playerAGteScore()
-
-    // Act
-    let result = app.echo()
-
-    // Assert
-    expect(result).toBe('Fifteen - Love')
-})
-
-test('Echo "Love - Fifteen" when playerB get first score', () => {
-    // Arrange
-    let app = new TennisGame()
-    app.reset()
-    app.playerBGteScore()
-
-    // Act
-    let result = app.echo()
-
-    // Assert
-    expect(result).toBe('Love - Fifteen')
-})
-
-test('Echo "Thirty - Fifteen" when score is 30 - 15 ', () => {
-    // Arrange
-    let app = new TennisGame()
-    app.reset()
-    app.playerBGteScore()
-    app.playerAGteScore()
-    app.playerAGteScore()
-
-    // Act
-    let result = app.echo()
-
-    // Assert
-    expect(result).toBe('Thirty - Fifteen')
-})
+// test('Echo "Love - Love" when game start', () => {
+//     // Arrange
+//     let app = new TennisGame()
+//
+//     // Act
+//     app.reset()
+//     let result = app.echo()
+//
+//     // Assert
+//     expect(result).toBe('Love - Love')
+// })
+//
+// test('Echo "Fifteen - Love" when playerA get first score', () => {
+//     // Arrange
+//     let app = new TennisGame()
+//     app.reset()
+//     app.playerAGetScore()
+//
+//     // Act
+//     let result = app.echo()
+//
+//     // Assert
+//     expect(result).toBe('Fifteen - Love')
+// })
+//
+// test('Echo "Love - Fifteen" when playerB get first score', () => {
+//     // Arrange
+//     let app = new TennisGame()
+//     app.reset()
+//     app.playerBGetScore()
+//
+//     // Act
+//     let result = app.echo()
+//
+//     // Assert
+//     expect(result).toBe('Love - Fifteen')
+// })
+//
+// test('Echo "Thirty - Fifteen" when score is 30 - 15 ', () => {
+//     // Arrange
+//     let app = new TennisGame()
+//     app.reset()
+//     app.playerBGetScore()
+//     app.playerAGetScore()
+//     app.playerAGetScore()
+//
+//     // Act
+//     let result = app.echo()
+//
+//     // Assert
+//     expect(result).toBe('Thirty - Fifteen')
+// })
